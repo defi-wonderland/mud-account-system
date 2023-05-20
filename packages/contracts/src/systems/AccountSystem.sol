9aa9d54df02932c788791b5cddcc44a0e86a194d
Skeletor Spaceman <skeletorspaceman@gmail.com>
2023-05-20 12:14:56 -0300

    feat: add MUD template

---
+++ packages/contracts/src/systems/AccountSystem.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import { System } from "@latticexyz/world/src/System.sol";
+import { Counter } from "../codegen/Tables.sol";
+
+contract AccountSystem is System {
+  function createAccount() public returns (uint32) {
+    
+  }
+
+  function linkAccount(address _account) public returns (bytes32 _accountId) {
+    // recover signature
+  }
+
+}
