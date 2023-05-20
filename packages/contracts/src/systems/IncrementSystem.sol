259ef5d33127958351531d714c08988be7efc770
Skeletor Spaceman <skeletorspaceman@gmail.com>
2023-05-20 12:23:53 -0300

    feat: abstract on counter system

--- packages/contracts/src/systems/IncrementSystem.sol
+++ packages/contracts/src/systems/IncrementSystem.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import { System } from "@latticexyz/world/src/System.sol";
 import { Counter } from "../codegen/Tables.sol";
 
-contract IncrementSystem is System {
+contract IncrementSystem is AccountPermissionSystem {
   function increment() public returns (uint32) {
     uint32 counter = Counter.get();
     uint32 newValue = counter + 1;
