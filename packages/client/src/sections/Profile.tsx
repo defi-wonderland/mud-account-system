8c5d37f08bc53201013c74621f0914221ef43556
Skeletor Spaceman <skeletorspaceman@gmail.com>
2023-05-21 18:42:53 -0300

    feat: add ui component for burner and account

--- packages/client/src/sections/Profile.tsx
+++ packages/client/src/sections/Profile.tsx
@@ -1,12 +1,14 @@
 import { useMUD } from "../MUDContext";
 
 interface ProfileProps {
+  signer?: string;
   account?: string;
+  burner?: string;
   chainId?: number;
   connect?: () => void;
 }
 
-export const Profile = ({ account, chainId, connect }: ProfileProps) => {
+export const Profile = ({ account, burner, signer, chainId, connect }: ProfileProps) => {
   const {
     systemCalls: { createAccount },
   } = useMUD();
@@ -16,15 +18,21 @@ export const Profile = ({ account, chainId, connect }: ProfileProps) => {
       <h1>Profile Section</h1>
       <br />
       <br />
-      <button onClick={connect} disabled={!account}>
-        {account ? "Connected" : "Connect"}
+      <button onClick={connect} disabled={!signer}>
+        {signer ? "Connected" : "Connect"}
       </button>
       <br />
       <br />
-      <p>Account address: {account}</p>
+      <p>Signer address: {signer}</p>
       <p>ChainId: {chainId}</p>
       <br />
+      <p>Burner address: {burner}</p>
+      <br />
       <button onClick={createAccount}>Create Account</button>
+      <br />
+      <br />
+      <p>Signer Accounts:</p>
+      <p>Account address: {account}</p>
       <br />
     </div>
   );
