b3263067f3d74bd5579aac438cab73318025f667
Ardy <ardy@defi.sucks>
2023-05-22 21:38:25 -0400

    feat: add counter

--- packages/client/src/sections/Game.tsx
+++ packages/client/src/sections/Game.tsx
@@ -1,22 +1,40 @@
-import { useComponentValue, useRows } from "@latticexyz/react";
 import { useMUD } from "../MUDContext";
 import { useEntityQuery } from "@latticexyz/react";
 import { Has, getComponentValueStrict } from "@latticexyz/recs";
 import { useDataContext } from "../context";
+import { useEffect, useState } from "react";
 
 export const Game = () => {
   const {
-    components: { CounterGame, AccountFactorySingleton },
-    network: { singletonEntity, storeCache },
+    components: { CounterGame },
     systemCalls: { increment },
   } = useMUD();
   const { actionEnv } = useDataContext();
+  const [counter, setCounter] = useState(0);
+  const counterKey = useEntityQuery([Has(CounterGame)]);
 
+  const getCounter = async () => {
+    try {
+      const counterValue = getComponentValueStrict(
+        CounterGame,
+        counterKey[0]
+      ).counter;
+
+      setCounter(counterValue);
+    } catch (error) {
+      console.log("error gettting counter");
+    }
+  };
+
+  useEffect(() => {
+    getCounter();
+  }, [counterKey]);
   return (
     <div className="section">
       <h1>Game Section</h1>
       <br />
       <br />
+      <p>Counter: {counter || "??"}</p>
       <br />
       <button
         type="button"
